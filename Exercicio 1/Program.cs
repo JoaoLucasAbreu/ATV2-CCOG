b8e3643a5dce62a7c1f5b64193c45aed43486d22
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2023-11-21 20:25:21 -0300

    up0

--- Exercicio 1/Program.cs
+++ Exercicio 1/Program.cs
@@ -3,108 +3,53 @@
 namespace Projeto {
 	class Program {
 
-		readonly int[] quadrado = {
-			0, 0, 0, 0,
-			0, 255, 255, 0,
-			0, 255, 255, 0,
-			0, 0, 0, 0
-		};
-		static void Main(string[] args) {
+		static void Main(string[] args){
 
-			using (
-				SKBitmap bitmap = SKBitmap.Decode("Exercicio 1.png")) {
-				Console.WriteLine(bitmap.ColorType);
+			using (SKBitmap bitmap = SKBitmap.Decode("Exercicio 1.png")){
+    			int largura = bitmap.Width;
+				int altura = bitmap.Height;
+                int count = 0;
 
-				unsafe {
+				byte[] forma = new byte[]{
+					0, 0, 0, 0,
+					0, 255, 255, 0,
+					0, 255, 255, 0,
+					0, 0, 0, 0
+				};
+				unsafe{
+					byte* entrada = (byte*)bitmap.GetPixels();
+                    count = check(entrada, forma, largura, altura);
+				}
 
-					int largura = bitmap.Width;
-					int altura = bitmap.Height;
-
-					byte* ptr = (byte*)bitmap.GetPixels();
-					int count  = 0;
-					bool check;
-
-					for (int y = 0; y < altura; y++)
-					{
-						for (int x = 0; x < largura; x++)
-						{
-							if (ptr[y * largura + x] == 255)
-							{
-								check = Check(ptr, x, y, largura, altura);
-								if (check)
-									Console.Write("oi");
+				Console.WriteLine("Número de aparições: " + count);
+			}
+		}
+		
+        static unsafe int check(byte* entrada, byte[] forma, int altura, int largura){
+            int contador=0;
+            	for (int y = 0; y < altura - 3; y++){
+					for (int x = 0; x < largura - 3; x++){
+						bool achou_forma = true;
+						    for (int py = 0; py < 4; py++){
+								for (int px = 0; px < 4; px++){
+									byte* pixel_atual = entrada + ((y + py) * largura + (x + px));
+									byte valor_pixel = pixel_atual[0];
+									if (valor_pixel != forma[py * 4 + px]){
+										achou_forma = false;
+										break;
+									}
+								}
+								if (!achou_forma){
+									break;
+								}
+							}
+							if (achou_forma){
+								contador++;
 							}
 						}
 					}
 
-					Console.WriteLine("Número de aparições: " + count);
-				
-				}
-
-			}
-		}
-		
-
-		static unsafe bool Check(byte* img, int x, int y, int largura, int altura) {
-
-			if (x - 1 < 0) 
-				return false;
-			
-			if (y - 1 < 0) 
-				return false;
-
-			if (x + 3 > largura - 1)
-				return false;
-
-			if (y + 3 > altura - 1)
-				return false;
-			
-			x--;
-			y--;
-			int[] quadrado2 = new int[16];
-
-			if (img[y * largura + x] != 0)
-				return false;
-			else {
-			}
-
-
-
-
-
-			if ()
-
-
-
-
-
-
-
-			for (int a = 0; a < 4; a++)
-			{
-				for (int l = 0; l < 4; l++) 
-				{
-					if (a == 0 || a == 3 || l == 0 || l == 3)
-					{
-						if (img[((y + a) * largura) + x + l] != 0) 
-						{
-							Console.WriteLine("oi");
-							return false;
-						}
-
-					} else {
-						if (img[(y + a) * largura + x + l] != 255) 
-						{
-							Console.WriteLine("tchau");
-							return false;	
-						}
-					}
-				}
-			}
-			
-			return true;
-
-		}
-
+            return contador;
+        }
 	}
 }
